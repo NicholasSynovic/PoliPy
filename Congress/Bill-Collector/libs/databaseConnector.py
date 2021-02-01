efa0923ca492720a51d5375465689564332c5776
Nicholas M. Synovic <Nicholas.Synovic@GMail.com>
2021-02-01 21:48:13 +0000

    Add database connector

---
+++ Congress/Bill-Collector/libs/databaseConnector.py
@@ -0,0 +1,40 @@
+import sqlite3
+from os import error
+from sqlite3 import Connection, OperationalError
+
+
+class DatabaseConnector:
+    def __init__(self, databaseFileName: str) -> None:
+        self.file = databaseFileName
+        self.createDatabase()
+        self.databaseConnection = sqlite3.connect(self.file)
+
+    def createDatabase(self) -> bool:
+        try:
+            with open(self.file, "r") as database:
+                database.close()
+            return False
+        except FileNotFoundError:
+            with open(self.file, "w") as database:
+                database.close()
+            return True
+
+    def executeSQL(
+        self,
+        sql: str,
+        options: tuple = None,
+    ) -> bool:
+        connection = self.databaseConnection
+        try:
+            if options is None:
+                connection.execute(sql)
+            else:
+                connection.execute(sql, options)
+        except OperationalError as error:
+            return False
+        connection.commit()
+        return True
+
+    def closeDatabaseConnection(self, databaseConnection: Connection) -> bool:
+        databaseConnection.close()
+        return True
