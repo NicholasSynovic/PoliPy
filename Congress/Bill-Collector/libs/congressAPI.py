b0663224927f2f8d18b167bcfa124ad4b7d8ba5f
Nicholas M. Synovic <Nicholas.Synovic@GMail.com>
2021-02-01 20:46:57 +0000

    Create the basic api to access Congress.gov

---
+++ Congress/Bill-Collector/libs/congressAPI.py
@@ -0,0 +1,20 @@
+from json import dumps
+
+import requests
+from bs4 import BeautifulSoup
+from requests import Response
+
+
+class CongressAPI:
+    def __init__(self, **kwargs) -> None:
+        self.query = kwargs
+        self.url = (
+            "https://www.congress.gov/search?searchResultViewType=expanded&q={}".format(
+                dumps(self.query)
+            )
+        )
+
+    def sendRequest(self) -> list:
+        req = requests.get(self.url)
+        soup = BeautifulSoup(markup=req.content, features="lxml")
+        return [soup, req]
---
+++ Congress/Bill-Collector/libs/test_congressAPI.py
@@ -0,0 +1,34 @@
+import unittest
+
+import bs4
+import requests
+
+import congressAPI
+
+
+class Test_CongressAPI(unittest.TestCase):
+    def test_Init(self):
+        c1 = congressAPI.CongressAPI()
+        c2 = congressAPI.CongressAPI(test1="test1", test2=["test2", "test3"])
+        self.assertEqual({}, c1.query)
+        self.assertEqual(
+            r"https://www.congress.gov/search?searchResultViewType=expanded&q={}",
+            c1.url,
+        )
+        self.assertEqual("test1", c2.query["test1"])
+        self.assertEqual(["test2", "test3"], c2.query["test2"])
+        self.assertEqual(
+            r'https://www.congress.gov/search?searchResultViewType=expanded&q={"test1": "test1", "test2": ["test2", "test3"]}',
+            c2.url,
+        )
+
+    def test_SendRequest(self):
+        c3 = congressAPI.CongressAPI().sendRequest()
+        self.assertIs(list, type(c3))
+        self.assertIs(bs4.BeautifulSoup, type(c3[0]))
+        self.assertIs(requests.models.Response, type(c3[1]))
+        self.assertEqual(200, c3[1].status_code)
+
+
+if __name__ == "__main__":
+    unittest.main()
