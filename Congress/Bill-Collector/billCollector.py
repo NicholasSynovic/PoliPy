841bcc5dc2173cb42256e5477e3d5b5c06efdc5d
Nicholas M. Synovic <Nicholas.Synovic@GMail.com>
2021-02-03 03:27:58 +0000

    Add tests

--- Congress/Bill-Collector/billCollector.py
+++ Congress/Bill-Collector/billCollector.py
@@ -1,1 +1,14 @@
 from libs import congressAPI, databaseConnector, scraper
+
+
+class BillCollector:
+    def __init__(self, **kwargs) -> None:
+        self.c = congressAPI.CongressAPI(kwargs)
+        self.d = databaseConnector.DatabaseConnector(
+            databaseFileName=kwargs["congress"]
+        )
+        self.s = None
+
+    def createScraper(self) -> None:
+        soup = self.c.sendRequest()[0]
+        self.s = scraper.Scraper(soup=soup)
--- Congress/Bill-Collector/libs/congressAPI.py
+++ Congress/Bill-Collector/libs/congressAPI.py
@@ -5,15 +5,20 @@ from bs4 import BeautifulSoup
 
 
 class CongressAPI:
-    def __init__(self, **kwargs) -> None:
+    def __init__(self, **kwargs) -> None:  # TODO: Fix test
+        self.page = 1
         self.query = kwargs
-        self.url = (
-            "https://www.congress.gov/search?searchResultViewType=expanded&q={}".format(
-                dumps(self.query)
-            )
+        self.url = "https://www.congress.gov/search?searchResultViewType=expanded&page={}&q={}".format(
+            self.page, dumps(self.query)
         )
 
     def sendRequest(self) -> list:
         req = requests.get(self.url)
         soup = BeautifulSoup(markup=req.content, features="lxml")
         return [soup, req]
+
+    def incrementPage(self) -> None:  # TODO: Write test
+        self.page += 1
+        self.url = "https://www.congress.gov/search?searchResultViewType=expanded&page={}&q={}".format(
+            self.page, dumps(self.query)
+        )
